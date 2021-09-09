254ffd0d7119dc6cb7c930c2775c4411b500ec09
bathelomeo <ngethenathaniel@gmail.com>
2021-09-09 18:15:12 +0400

    Added new files

---
+++ demo.py
@@ -0,0 +1,16 @@
+Nathaniel = {}
+Nathaniel['first'] = 'Nathaniel'
+Nathaniel['last']= 'Ngethe'
+
+Arthur = {}
+Arthur['first'] = 'Arthur'
+Arthur['last'] = 'Joe'
+
+people = []
+people.append(Nathaniel)
+people.append(Arthur)
+people.append({
+    'first': 'Margaret', 'last': 'Ngethe'
+})
+
+print(people)
---
+++ demos.py
@@ -0,0 +1,12 @@
+people = ['Nathaniel', 'Ngethe']
+
+print()
+for name in people:
+    print(name)
+
+index = 0
+while index < len(people):
+   print(people[index])
+   index = index + 1
+
+print()
